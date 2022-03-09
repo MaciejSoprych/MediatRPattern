48f5ef2d630197573ab14708df72a2e166b46509
TPA\maciej.soprych <maciej.soprych@tpa-group.pl>
2022-03-09 20:30:06 +0100

    Dodaj pliki projektów.

---
+++ MediatRPattern/Controllers/Currency.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatRPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Currency : ControllerBase
+    {
+        private readonly IMediator _mediatR;
+
+        public Currency(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        [HttpGet("api/currencies")]
+        public async Task<List<CurrencyDto>> Get()
+        {
+            return await _mediatR.Send(new Query());
+        }
+        public class Query : IRequest<List<CurrencyDto>>
+        {
+
+        }
+
+        public class CurrencyDto
+        {
+            public CurrencyDto(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+        }
+
+        public class GetCurrencyQueryHandler : IRequestHandler<Query, List<CurrencyDto>>
+        {
+            public async Task<List<CurrencyDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var currencies = new List<CurrencyDto>
+                {
+                    new CurrencyDto("PLN"),
+                    new CurrencyDto("EUR"),
+                    new CurrencyDto("USD")
+                };
+
+                return currencies;
+            }
+        }
+    }
+}
